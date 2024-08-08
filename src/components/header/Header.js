efa1df698e93f204003c44a070212a778143d12e
Michał Galiński <michalg359@gmail.com>
2024-08-08 16:41:43 +0200

    work on dynamic nav-bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -48,3 +48,5 @@ export default function Header() {
     </header>
   );
 }
+
+function handleOnClick() {}
--- src/components/header/NavItem.js
+++ src/components/header/NavItem.js
@@ -1,6 +1,11 @@
 export default function NavItem({ itemObj }) {
   return (
-    <li>
+    <li
+      onClick={(e) => {
+        // e.target.classList.remove("nav-item--active");
+        e.target.classList.add("nav-item--active");
+      }}
+    >
       {itemObj.icon}&nbsp;&nbsp;{itemObj.name}
     </li>
   );
